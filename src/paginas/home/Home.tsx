760244a77a906098847ca23d7b7aeff5b3e51253
dogzeira <douglasdanilobutu13@gmail.com>
2022-05-24 21:34:48 -0300

    Implementou react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,22 @@
+ import React from "react";
+ import "./Home.css";
+
+// tag de fragmento <>
+
+function Home(){
+
+    return( <>
+                 <div className="img-fundo">
+                     
+                 
+                 <h1 className="titulo">Home</h1>
+                 /* <img className="img" src="https://th.bing.com/th/id/R.782318d62939beb1b2546840879d0187?rik=STq89pCWfHKctg&riu=http%3a%2f%2fwww.freepngimg.com%2fdownload%2fone_piece%2f23457-1-one-piece-luffy-transparent-background.png&ehk=TjEo5fTXaEABujPI%2bf9WBwVFSQBy11bGgK8d0zCjFcA%3d&risl=1&pid=ImgRaw&r=0" alt="imagem one piece"/> 
+              
+                 </div>
+            </>
+
+    );
+
+}
+
+export default Home;
