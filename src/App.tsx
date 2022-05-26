2e2c2bb63b49babd124cf4024cb06e7cca7dc120
dogzeira <douglasdanilobutu13@gmail.com>
2022-05-25 21:07:39 -0300

    Criou os componentes Menu, Rodape, Home através do Material Ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import Home from './paginas/home/Home';
-
+import Navbar from "./componentes/estaticos/navbar/Navbar"
+import Footer from "./componentes/estaticos/footer/Footer"
 import './App.css';
+
+
 
 
 function App() {
 
   return (
-
-    <Home />
-);
+  <>
+  <div className="img-fundo">
+    <Navbar/>
+    <Home/>
+    <Footer/>
+    </div>
+    </>
+   
+  
+  );
 }
-
 export default App;
